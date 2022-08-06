e4232f356a3a59437d4e7b5fdd7b5793b14b786d
tauri_s <tauristclaire@gmail.com>
2022-08-05 17:05:41 -0700

    Intro to constant let and scope

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// add button
+const addShowButton = document.querySelector(".add");
+// label for the add button
+const showInput = document.querySelector(".add-show input");
+// unordered list (not yet visible)
+const showList = document.querySelector(".show-list");
+// span class for number of favorite shows
+const showCount = document.querySelector(".number");
+
+//The value property captures user input
+//if parameter checks that input is not blank
+//Create new list item
+//Change innertext to show variable
+//use append to add the item to the end of the unordered list
+//Select all elements in document with variable shows
+//Display number of elements in number class
+addShowButton.addEventListener("click", function () {
+    const show = showInput.value;
+    if (show !== "") {
+        let listItem = document.createElement("li");
+        listItem.innerText = show;
+        showList.append(listItem);
+        let shows = document.querySelectorAll(".show-list li");
+        showCount.innertext = shows.length;
+    }
+});
